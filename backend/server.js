6360392cf26f39ff36d11b0bd7b82e49a01a6cc6
Ismail Khan <ismailzkhan161@gmail.com>
2026-09-17 23:55:38 +0500

    fix errorhandler.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 
 const connectDB = require('./config/Db');
-const errorHandler = require('./middleware/errorhandler.Js');
+const errorHandler = require('./middleware/errorhandler.js');
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
