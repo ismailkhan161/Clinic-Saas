6b7f7ec981b4f6ac94e8836946990d4eba096e31
Ismail Khan <ismailzkhan161@gmail.com>
2026-09-17 21:12:35 +0500

    fix changes in correct way

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,9 @@ connectDB();
 app.use(helmet());
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
